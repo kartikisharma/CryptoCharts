d7cf98e5970f3bcc822f1f0ba68f2d80a48aaf35
Kartiki Sharma <k35sharm@uwaterloo.ca>
2018-02-03 15:14:06 -0500

    Last changes before adding in rxJava

--- CryptoCharts/app/src/main/java/kartiki/cryptocharts/MainActivity.java
+++ CryptoCharts/app/src/main/java/kartiki/cryptocharts/MainActivity.java
@@ -1,15 +1,13 @@
 package kartiki.cryptocharts;
 
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
